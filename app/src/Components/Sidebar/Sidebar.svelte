b1fc881bac465acba4fd00706cd36695981ba1c7
theseyan <sayanjyotidasworkmail@gmai.com>
2024-10-02 19:22:54 +0530

    update

--- app/src/Components/Sidebar/Sidebar.svelte
+++ app/src/Components/Sidebar/Sidebar.svelte
@@ -71,13 +71,13 @@
             icon: `<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M12.01 6.001C6.5 1 1 8 5.782 13.001L12.011 20l6.23-7C23 8 17.5 1 12.01 6.002Z"/></svg>`,
             iconActive: `<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24"><path d="m12.75 20.66 6.184-7.098c2.677-2.884 2.559-6.506.754-8.705-.898-1.095-2.206-1.816-3.72-1.855-1.293-.034-2.652.43-3.963 1.442-1.315-1.012-2.678-1.476-3.973-1.442-1.515.04-2.825.76-3.724 1.855-1.806 2.201-1.915 5.823.772 8.706l6.183 7.097c.19.216.46.34.743.34a.985.985 0 0 0 .743-.34Z"/></svg>`
         },
-        // {
-        //     label: `About`,
-        //     route: `/about`,
-        //     descending: false,
-        //     icon: `<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/></svg>`,
-        //     iconActive: `<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm9.408-5.5a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2h-.01ZM10 10a1 1 0 1 0 0 2h1v3h-1a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-1v-4a1 1 0 0 0-1-1h-2Z" clip-rule="evenodd"/></svg>`
-        // },
+        {
+            label: `About`,
+            route: `/about`,
+            descending: false,
+            icon: `<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M10 11h2v5m-2 0h4m-2.592-8.5h.01M21 12a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"/></svg>`,
+            iconActive: `<svg class="w-6 h-6 text-gray-800 dark:text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" viewBox="0 0 24 24"><path fill-rule="evenodd" d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm9.408-5.5a1 1 0 1 0 0 2h.01a1 1 0 1 0 0-2h-.01ZM10 10a1 1 0 1 0 0 2h1v3h-1a1 1 0 1 0 0 2h4a1 1 0 1 0 0-2h-1v-4a1 1 0 0 0-1-1h-2Z" clip-rule="evenodd"/></svg>`
+        },
     ];
 </script>
 
--- app/src/Pages/About/About.svelte
+++ app/src/Pages/About/About.svelte
@@ -17,10 +17,15 @@
         <h1 class="heading">About</h1>
 
         <div class="font-medium">
+            Tracking mental health by manually specifying the severity of various emotions is a tedious task.
+            We use AI that understands natural language and human emotions to "automagically" track and records mood statistics.
+            <br /><br />
+            Our AI therapist provides immediate feedback, analysis and identification of potential issues on every user input.
+            <br /><br />
             MindTraq is our go at solving Problem 20 of Gen AI Exchange Hackathon by Google: 
             Gemini-Enhanced AI for Mental Health & Emotional Support.
             <br /><br />
-            Developed by Team MindTraq, consisting of:
+            Developed by Team MindTraq hailing from National Institute of Technology (NIT) Agartala, consisting of:
             <ul class="list-disc mx-4">
                 <li><b>Sayan Jyoti Das</b> (CSE '27)</li>
                 <li><b>Shivam Kumar</b> (CE '26)</li>
